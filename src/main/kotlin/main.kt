7ac2c9c858a0aaea8b86169ba7829ec893d6d3c8
Timur Anvartdinov <t.anvartdinov@life-stream.tv>
2024-05-03 21:39:20 +0300

    fix problem with negative purchaseAmount

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,14 +2,17 @@ import kotlin.math.ceil
 
 fun main() {
     val isRegularCustomer = true
-    val purchaseAmount = 11901
+    val purchaseAmount = -11901
 
-    val discount = when (purchaseAmount) {
-        in 0..1_000 -> 0
-        in 1_001..10_000 -> 100
-        else -> purchaseAmount * 5 / 100
+    if (purchaseAmount < 0) {
+        println("Нельзя оформить покупку с отрицательной суммой")
+    } else {
+        val discount = when (purchaseAmount) {
+            in 0..1_000 -> 0
+            in 1_001..10_000 -> 100
+            else -> purchaseAmount * 5 / 100
+        }
+        val amountDue = (purchaseAmount - discount) * (if (isRegularCustomer) 0.99 else 1.0)
+        println("Сумма к оплате - ${ceil(amountDue)}")
     }
-
-    val amountDue = (purchaseAmount - discount) * (if (isRegularCustomer) 0.99 else 1.0)
-    println("Сумма к оплате - ${ceil(amountDue)}")
 }
